5dbb56f09b3a3def9feb5d4656e0e9faa5e02404
AgaevAlex <agaevalex97@gmail.com>
2022-05-04 00:45:51 +0300

    HW10

--- src/ru/agaev/webapp/storage/serializer/DataStreamSerializer.java
+++ src/ru/agaev/webapp/storage/serializer/DataStreamSerializer.java
@@ -23,53 +23,35 @@ public class DataStreamSerializer implements StreamSerializer {
             });
 
             Map<SectionType, Section> sections = resume.getSections();
-            writeWithExeption(sections.entrySet(), dos, entry->{
+            writeWithExeption(sections.entrySet(), dos, entry -> {
                 SectionType sectionType = entry.getKey();
+                dos.writeUTF(entry.getKey().name());
                 switch (sectionType) {
                     case OBJECTIVE:
                     case PERSONAL:
-                        dos.writeUTF(entry.getKey().name());
-                        dos.writeUTF(entry.getValue().toString());
+                        dos.writeUTF(((TextSection) entry.getValue()).getText());
                         break;
                     case ACHIEVEMENT:
                     case QUALIFICATIONS:
-                        dos.writeUTF(entry.getKey().name());
                         List<String> list = ((ListSection) entry.getValue()).getList();
-                        dos.writeInt(list.size());
-                        for (String s : list) {
-                            dos.writeUTF(s);
-                        }
+                        writeWithExeption(list, dos, dos::writeUTF);
                         break;
                     case EXPERIENCE:
                     case EDUCATION:
-                        dos.writeUTF(entry.getKey().name());
-                        List<Organization> organizations = ((OrganizationSection) entry.getValue()).getExperience();
-                        dos.writeInt(organizations.size());
-                        organizations.forEach(organization -> serializeOrganizations(organization, dos));
+                        writeWithExeption(((OrganizationSection) entry.getValue()).getExperience(), dos, entry1 -> {
+                            Link link = entry1.getHomePage();
+                            dos.writeUTF(link.getName());
+                            dos.writeUTF(link.getUrl());
+                            writeWithExeption(entry1.getExperiences(), dos, entry2 -> {
+                                dos.writeUTF(String.valueOf(entry2.getStartDate()));
+                                dos.writeUTF(String.valueOf(entry2.getEndDate()));
+                                dos.writeUTF(entry2.getTitle());
+                                dos.writeUTF(entry2.getDescription());
+                            });
+                        });
                         break;
                 }
             });
-        }
-    }
-
-    private void serializeOrganizations(Organization organization, DataOutputStream dos) {
-        Link link = organization.getHomePage();
-        try {
-            dos.writeUTF(link.getName());
-            dos.writeUTF(link.getUrl());
-            serializeExperience(organization.getExperiences(), dos);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void serializeExperience(List<Experience> experiences, DataOutputStream dos) throws IOException {
-        dos.writeInt(experiences.size());
-        for (Experience experience : experiences) {
-            dos.writeUTF(String.valueOf(experience.getStartDate()));
-            dos.writeUTF(String.valueOf(experience.getEndDate()));
-            dos.writeUTF(experience.getTitle());
-            dos.writeUTF(experience.getDescription());
         }
     }
 
@@ -77,12 +59,8 @@ public class DataStreamSerializer implements StreamSerializer {
     public Resume doRead(InputStream is) throws IOException {
         try (DataInputStream dis = new DataInputStream(is)) {
             Resume resume = new Resume(dis.readUTF(), dis.readUTF());
-            int size = dis.readInt();
-            for (int i = 0; i < size; i++) {
-                resume.addContact(ContactType.valueOf(dis.readUTF()), dis.readUTF());
-            }
-            size = dis.readInt();
-            for (int i = 0; i < size; i++) {
+            readWithExeption(dis, () -> resume.addContact(ContactType.valueOf(dis.readUTF()), dis.readUTF()));
+            readWithExeption(dis, () -> {
                 SectionType sectionType = SectionType.valueOf(dis.readUTF());
                 switch (sectionType) {
                     case OBJECTIVE:
@@ -91,50 +69,52 @@ public class DataStreamSerializer implements StreamSerializer {
                         break;
                     case ACHIEVEMENT:
                     case QUALIFICATIONS:
-                        resume.addSection(sectionType, new ListSection(deserializeList(dis)));
+                        resume.addSection(sectionType, new ListSection(collectionWithExeption(dis, dis::readUTF)));
                         break;
                     case EXPERIENCE:
                     case EDUCATION:
-                        resume.addSection(sectionType, new OrganizationSection(deserializeOrganization(dis)));
-                        break;
+                        resume.addSection(sectionType, new OrganizationSection(collectionWithExeption(dis, () -> {
+                            Organization organization = new Organization();
+                            organization.setHomePage(new Link(dis.readUTF(), dis.readUTF()));
+                            organization.setExperiences(collectionWithExeption(dis, () -> new Experience(LocalDate.parse(dis.readUTF()), LocalDate.parse(dis.readUTF()), dis.readUTF(), dis.readUTF())));
+                            return organization;
+                        })));
                 }
 
-            }
+            });
             return resume;
-
         }
     }
 
-    private List<Organization> deserializeOrganization(DataInputStream dis) throws IOException {
-        List<Organization> organizations = new ArrayList<>();
-        int counterOrganizations = dis.readInt();
-        for (int i = 0; i < counterOrganizations; i++) {
-            Organization organization = new Organization();
-            organization.setHomePage(new Link(dis.readUTF(), dis.readUTF()));
-            organization.setExperiences(deserializeExperience(dis));
-            organizations.add(organization);
-        }
+//    private List<Organization> deserializeOrganization(DataInputStream dis) throws IOException {
+//        List<Organization> organizations = new ArrayList<>();
+//        int counterOrganizations = dis.readInt();
+//        for (int i = 0; i < counterOrganizations; i++) {
+//            Organization organization = new Organization();
+//            organization.setHomePage(new Link(dis.readUTF(), dis.readUTF()));
+//            organization.setExperiences(deserializeExperience(dis));
+//            organizations.add(organization);
+//        }
+//        return organizations;
+//    }
+//
+//    private List<String> deserializeList(DataInputStream dis) throws IOException {
+//        int counterSection = dis.readInt();
+//        List<String> list = new ArrayList<>();
+//        for (int i = 0; i < counterSection; i++) {
+//            list.add(dis.readUTF());
+//        }
+//        return list;
+//    }
 
-        return organizations;
-    }
-
-    private List<String> deserializeList(DataInputStream dis) throws IOException {
-        int counterSection = dis.readInt();
-        List<String> list = new ArrayList<>();
-        for (int i = 0; i < counterSection; i++) {
-            list.add(dis.readUTF());
-        }
-        return list;
-    }
-
-    private List<Experience> deserializeExperience(DataInputStream dis) throws IOException {
-        List<Experience> list = new ArrayList<>();
-        int countExperience = dis.readInt();
-        for (int i = 0; i < countExperience; i++) {
-            list.add(new Experience(LocalDate.parse(dis.readUTF()), LocalDate.parse(dis.readUTF()), dis.readUTF(), dis.readUTF()));
-        }
-        return list;
-    }
+//    private List<Experience> deserializeExperience(DataInputStream dis) throws IOException {
+//        List<Experience> list = new ArrayList<>();
+//        int countExperience = dis.readInt();
+//        for (int i = 0; i < countExperience; i++) {
+//            list.add(new Experience(LocalDate.parse(dis.readUTF()), LocalDate.parse(dis.readUTF()), dis.readUTF(), dis.readUTF()));
+//        }
+//        return list;
+//    }
 
     private <T> void writeWithExeption(Collection<T> collection, DataOutputStream dos, WriteCollection<T> writeCollection) throws IOException {
         dos.writeInt(collection.size());
@@ -143,7 +123,31 @@ public class DataStreamSerializer implements StreamSerializer {
         }
     }
 
+    private <T> void readWithExeption(DataInputStream dis, ReadCollection<T> readCollection) throws IOException {
+        int size = dis.readInt();
+        for (int i = 0; i < size; i++) {
+            readCollection.read();
+        }
+    }
+
+    private <T> List<T> collectionWithExeption(DataInputStream dis, ReturnCollection<T> returnCollection) throws IOException {
+        List<T> list = new ArrayList<>();
+        int counter = dis.readInt();
+        for (int i = 0; i < counter; i++) {
+            list.add(returnCollection.read());
+        }
+        return list;
+    }
+
     interface WriteCollection<T> {
         void write(T t) throws IOException;
+    }
+
+    interface ReadCollection<T> {
+        void read() throws IOException;
+    }
+
+    interface ReturnCollection<T> {
+        T read() throws IOException;
     }
 }
