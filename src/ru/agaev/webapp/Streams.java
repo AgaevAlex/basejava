96e8de68a89b1fe730d023b34ac96cfadb4a127b
AgaevAlex <agaevalex97@gmail.com>
2022-05-17 03:56:57 +0300

    HW12

--- src/ru/agaev/webapp/Streams.java
+++ src/ru/agaev/webapp/Streams.java
@@ -3,6 +3,7 @@ package ru.agaev.webapp;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class Streams {
@@ -11,10 +12,9 @@ public class Streams {
         List<Integer> list = new ArrayList<>();
         list.add(1);
         list.add(2);
+        list.add(3);
+        list.add(4);
         list.add(5);
-        list.add(6);
-        list.add(3);
-        list.add(3);
         System.out.println(minValue(i));
         System.out.println(oddOrEven(list).toString());
     }
@@ -30,10 +30,15 @@ public class Streams {
                         : integers.stream().filter(a -> a % 2 != 0).collect(Collectors.toList())).orElse(new ArrayList<>());
     }*/
 
+    /*    public static List<Integer> oddOrEven(List<Integer> integers) {
+            int count = integers.stream().mapToInt(Integer::intValue).sum() % Map<Boolean, List<Integer>> abcd = 2;
+            return integers.stream().filter(i -> i % 2 != count).collect(Collectors.toList());
+        }*/
     public static List<Integer> oddOrEven(List<Integer> integers) {
-        int count = integers.stream().mapToInt(Integer::intValue).sum() % 2;
-        return integers.stream().filter(i -> i % 2 != count).collect(Collectors.toList());
-
+        //        Map<Boolean, List<Integer>> abcd = integers.stream().map(i -> i % 2 == 0 ? abcd.put(true, i) : abcd.put(false, i));
+        Map<Boolean, List<Integer>> abcd = integers.stream().collect(Collectors.partitioningBy(i -> i % 2 == 0));
+        return abcd.get(false).size() % 2 == 0 ? abcd.get(false) : abcd.get(true);
     }
+
 }
 
